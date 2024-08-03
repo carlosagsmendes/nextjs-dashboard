9003e46563c4d2b260e001580e2ba750d715eb84
carlosagsmendes <carlos.ags.mendes@gmail.com>
2024-08-04 00:43:44 +0100

    exports

--- app/ui/fonts.tsx
+++ app/ui/fonts.tsx
@@ -1,7 +1,9 @@
 import { Inter, Lusitana } from 'next/font/google';
 
-export const inter = Inter({ subsets: ['latin'] });
-export const lusitana = Lusitana({
+const inter = Inter({ subsets: ['latin'] });
+const lusitana = Lusitana({
     weight: ['400', '700'],
     subsets: ['latin'],
 });
+
+export { inter, lusitana }
